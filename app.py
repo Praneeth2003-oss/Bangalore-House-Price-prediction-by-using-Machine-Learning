89e12f9fbc73aa4af8a39185db63c385c007b67e
Praneeth2003-oss <praneeth2003g@gmail.com>
2025-11-22 01:32:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def predict():
 
 if __name__ == "__main__":
     # for development use; in production use a WSGI server
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
